5b5dbc89c9b50ae66a547c7f555a42e06688298e
Rayane-Aboud <jr_aboud@esi.dz>
2025-12-29 01:05:45 +0200

    organised handle connection

--- supervisor/src/utils.rs
+++ supervisor/src/utils.rs
@@ -1,9 +1,68 @@
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 
 use common::{Message, serialize};
-use tokio::{io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader}, net::TcpStream, sync::broadcast, time::Instant};
+use tokio::{io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader}, net::{TcpStream, tcp::{OwnedReadHalf, OwnedWriteHalf}}, sync::{Mutex, broadcast}, time::Instant};
 
 use crate::worker_node::{WorkerInfo, WorkerRegistry};
+
+
+async fn register_worker(
+    workers: &Mutex<HashMap<String, WorkerInfo>>,
+    addr: SocketAddr,
+) {
+    let mut map = workers.lock().await;
+    map.insert(
+        addr.to_string(),
+        WorkerInfo {
+            addr,
+            connected_at: Instant::now(),
+        },
+    );
+}
+
+async fn remove_worker (
+    workers: &Mutex<HashMap<String, WorkerInfo>>,
+    addr: SocketAddr,
+){
+    let mut map = workers.lock().await;
+    map.remove(&addr.to_string());
+}
+
+async fn handle_read(
+    reader: &mut BufReader<OwnedReadHalf>,
+    buffer: &mut String,
+    tx: &broadcast::Sender<String>,
+    addr: &str,
+) -> bool {
+    match reader.read_line(buffer).await.unwrap_or(0) {
+        0 => {
+            let _ = tx.send(serialize(Message::Disconnected(addr.to_string())));
+            false
+        }
+        _ => {
+            let _ = tx.send(serialize(Message::Broadcast(
+                buffer.trim().to_string(),
+            )));
+            buffer.clear();
+            true
+        }
+    }
+}
+
+
+async fn handle_write(
+    writer: &mut OwnedWriteHalf,
+    rx: &mut broadcast::Receiver<String>,
+) {
+    if let Ok(msg) = rx.recv().await {
+        let _ = writer
+            .write_all(format!("{}\n", msg).as_bytes())
+            .await;
+    }
+}
+
+
+
 
 pub async fn handle_connection(
     socket: TcpStream,
@@ -13,56 +72,26 @@ pub async fn handle_connection(
     workers: WorkerRegistry,
 ) {
 
-    {
-        // Register worker
-        let mut map = workers.lock().await;
-        map.insert(
-            addr.to_string(),
-            WorkerInfo {
-                addr,
-                connected_at: Instant::now(),
-            },
-        );
-    }
-
+    register_worker(&workers, addr).await;
 
     let (read, mut write) = socket.into_split();
     let mut reader = BufReader::new(read);
     let mut buffer = String::new();
-    
+    let addr_str = &addr.to_string();
     let _ = tx.send(serialize(Message::Connected(addr.to_string())));
 
     loop {
         tokio::select! {
-            r = reader.read_line(&mut buffer) => {
-                if r.unwrap_or(0) == 0 {
-                    let _ = tx.send(
-                        serialize(Message::Disconnected(addr.to_string()))
-                    );
-                    break;
-                }
-
-                let _ = tx.send(
-                    serialize(Message::Broadcast(buffer.trim().to_string()))
-                );
-
-                buffer.clear();
+            ok = handle_read(&mut reader, &mut buffer, &tx, addr_str) => {
+                if !ok { break; }
             }
 
-            m = rx.recv() => {
-                if let Ok(m) = m {
-                    let _ = write
-                        .write_all(format!("{}\n", m).as_bytes())
-                        .await;
-                }
-            }
+            _ = handle_write(&mut write, &mut rx) => {}
         }
     }
 
-    {
-        let mut map = workers.lock().await;
-        map.remove(&addr.to_string());
-    }
+    remove_worker(&workers,addr).await;
+    
     let _ = tx.send(serialize(Message::Disconnected(addr.to_string())));
 }
 
